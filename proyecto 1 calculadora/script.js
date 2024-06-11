1652737902203c2c6ef107c763120e414859ea69
Angel Sebastian Saccone Graneros <133783405+Angeluz88@users.noreply.github.com>
2024-06-11 16:07:22 -0300

    add calculator project
    simple and functional calculator created in htm5, javascript and styles wit sass css.

---
+++ proyecto 1 calculadora/script.js
@@ -0,0 +1,35 @@
+const display = document.querySelector("#display")
+
+const buttons = document.querySelectorAll("button")
+
+//math functions
+
+buttons.forEach((item)=>{
+    item.onclick=()=>{
+        if(item.id =="clear"){
+            display.innerText="";
+        }else if(item.id == "erase"){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length-1)
+        }else if (display.innerText !="" && item.id == "equal"){
+            display.innerText = eval(display.innerText);
+        }else if(display.innerText =="" && item.id== "equal"){
+            display.innerText = "Err";
+            setTimeout(()=>(display.innerText=""),1500);
+        }else{
+            display.innerText+=item.id;
+        }
+    };
+});
+
+//ligth and dark theme
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+
+let isDark =true;
+themeToggleBtn.onclick=()=>{
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active")
+    isDark!=isDark;
+}
